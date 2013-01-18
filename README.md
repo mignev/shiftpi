d2610526f81666c793558889cb913bb1315e4886
Marian Ignev <m.ignev@gmail.com>
2013-01-18 11:54:03 +0200

    implement execute method and pinsSetup

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
-import shiftpi
-
-## functions
-
-shiftpi.digitalWrite(pin, MODE)
-
-shiftpi.all(MODE)
-
-shiftpi.prepare(pin, MODE)
-shiftpi.apply()
-
-api ref http://bildr.org/2011/02/74hc595/
+More info soon :)
--- shiftpi.py
+++ shiftpi.py
@@ -1,3 +1,7 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
 # Define MODES
 ALL  = -1
 HIGH = 1
@@ -13,6 +17,17 @@ _registers = list()
 
 #How many of the shift registers - change this
 _number_of_shiftregisters = 1
+
+def pinsSetup(**kwargs):
+    if len(kwargs) > 0:
+        global _SER_pin, _RCLK_pin, _SRCLK_pin
+        _SER_pin = kwargs.get('ser', _SER_pin)
+        _RCLK_pin = kwargs.get('rclk', _RCLK_pin)
+        _SRCLK_pin = kwargs.get('srclk', _SRCLK_pin)
+
+    GPIO.setup(_SER_pin, GPIO.OUT)
+    GPIO.setup(_RCLK_pin, GPIO.OUT)
+    GPIO.setup(_SRCLK_pin, GPIO.OUT)
 
 def shiftregisters(num):
     global _number_of_shiftregisters
@@ -48,4 +63,17 @@ def digitalWrite(pin, mode):
     _execute()
 
 def _execute():
-    pass
+    all_pins = _all_pins()
+    GPIO.output(_RCLK_pin, GPIO.LOW)
+
+    for pin in range(all_pins -1, -1, -1):
+        GPIO.output(_SRCLK_pin, GPIO.LOW)
+
+        pin_mode = _registers[pin]
+
+        GPIO.output(_SER_pin, pin_mode)
+        GPIO.output(_SRCLK_pin, GPIO.HIGH)
+
+    GPIO.output(_RCLK_pin, GPIO.HIGH)
+
+pinsSetup()

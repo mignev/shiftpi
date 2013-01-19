6823802ccfa644e7fcd8130e5bc328424c1fd888
Marian Ignev <m.ignev@gmail.com>
2013-01-19 21:27:43 +0200

    add startupMode method

--- shiftpi.py
+++ shiftpi.py
@@ -21,36 +21,36 @@ _number_of_shiftregisters = 1
 def pinsSetup(**kwargs):
     if len(kwargs) > 0:
         global _SER_pin, _RCLK_pin, _SRCLK_pin
+
         _SER_pin = kwargs.get('ser', _SER_pin)
         _RCLK_pin = kwargs.get('rclk', _RCLK_pin)
         _SRCLK_pin = kwargs.get('srclk', _SRCLK_pin)
+
+        GPIO.cleanup()
 
     GPIO.setup(_SER_pin, GPIO.OUT)
     GPIO.setup(_RCLK_pin, GPIO.OUT)
     GPIO.setup(_SRCLK_pin, GPIO.OUT)
 
+def startupMode(mode, execute = False):
+    if isinstance(mode, int):
+        if mode is HIGH or mode is LOW:
+            _all(mode, execute)
+        else:
+            raise ValueError("The mode can be only HIGH or LOW or Dictionary with specific pins and modes")
+    elif isinstance(mode, dict):
+        for pin, mode in mode.iteritems():
+            _setPin(pin, mode)
+        if execute:
+            _execute()
+    else:
+        raise ValueError("The mode can be only HIGH or LOW or Dictionary with specific pins and modes")
+
+
 def shiftregisters(num):
     global _number_of_shiftregisters
     _number_of_shiftregisters = num
     _all(LOW)
-
-def _all_pins():
-    return _number_of_shiftregisters * 8
-
-def _all(mode):
-    all_shr = _all_pins()
-
-    for pin in range(0, all_shr):
-        _setPin(pin, mode)
-    _execute()
-
-    return _registers
-
-def _setPin(pin, mode):
-    try:
-        _registers[pin] = mode
-    except IndexError:
-        _registers.insert(pin, mode)
 
 def digitalWrite(pin, mode):
     if pin == ALL:
@@ -61,6 +61,25 @@ def digitalWrite(pin, mode):
 
         _setPin(pin, mode)
     _execute()
+
+def _all_pins():
+    return _number_of_shiftregisters * 8
+
+def _all(mode, execute = True):
+    all_shr = _all_pins()
+
+    for pin in range(0, all_shr):
+        _setPin(pin, mode)
+    if execute:
+        _execute()
+
+    return _registers
+
+def _setPin(pin, mode):
+    try:
+        _registers[pin] = mode
+    except IndexError:
+        _registers.insert(pin, mode)
 
 def _execute():
     all_pins = _all_pins()
